a87f61a48c8dd62a032611eec3a9d883c79a69d6
sutani_arisa <sutani_arisa@NS-M019.cnkyoto.local>
2020-05-29 11:40:32 +0900

    エラーと完了リンク先をhtmlへ変更

--- Unisilo/src/model/InsertUsers.java
+++ Unisilo/src/model/InsertUsers.java
@@ -94,9 +94,9 @@ public class InsertUsers extends HttpServlet {
 
 		   if(num == 1) {
 			   //リダイレクト
-		       response.sendRedirect("http://localhost:8080/Unisilo/UsersFinish.jsp");
+		       response.sendRedirect("htmls/finish.html");
 		   }else {
-			   response.sendRedirect("http://localhost:8080/Unisilo/UsersError.jsp");
+			   response.sendRedirect("htmls/error.html");
 		   }
 
 
