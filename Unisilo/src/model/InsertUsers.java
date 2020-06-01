0258905ec64a129b0020f24b3815404e7a03e1b0
Han <>
2020-06-01 11:49:57 +0900

    Revert "修正"
    This reverts commit 89cb0889a5323a5fb37b207dbfca8f7fa9898ad9.

--- Unisilo/src/model/InsertUsers.java
+++ Unisilo/src/model/InsertUsers.java
@@ -5,10 +5,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Enumeration;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
-@WebServlet("/InsertUsers")
+
 public class InsertUsers extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     //localhost変更に必要あり？データベース名・ユーザー・パスワードも変更必須→すべて完了・成功
@@ -24,6 +24,11 @@ public class InsertUsers extends HttpServlet {
 	private String user = "nskensyu2020";
 	private String pw = "2020Nskensyu!";
 
+
+    public InsertUsers() {
+        super();
+
+    }
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -39,33 +44,47 @@ public class InsertUsers extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 
 
+		Enumeration<String> names = request.getParameterNames();
+
 
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
 		String age = request.getParameter("age");
 
-		int num = 0;
-		Statement stmt=null;
-		Connection con = null;
-
 		try {
 
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
 
-            con = DriverManager.getConnection(url,user,pw);
-            stmt = con.createStatement();
-            String sql = "insert into users(name,gender,age) "
-                                + "values ('" + name + "','" + gender + "','" + age + "')";
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
 
-            num = stmt.executeUpdate(sql);
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
+		int num = 0;
+		Connection conn = null;
+		try {
+
+			conn = DriverManager.getConnection(url,user,pw);
+			Statement stmt = conn.createStatement();
+			String sql = "insert into users(name,gender,age) "
+						+ "values ('" + name + "','" + gender + "','" + age + "')";
+
+			num = stmt.executeUpdate(sql);
 
 
-		} catch (SQLException| ClassNotFoundException e ) {
-                e.printStackTrace();
+		} catch (SQLException e) {
+
+			e.printStackTrace();
 		}finally {
 			try {
-				con.close();
+				conn.close();
 			} catch (SQLException e) {
+
 				e.printStackTrace();
 			}
 		}
