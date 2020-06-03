5737e7a0fc552e5eff513cf05ac61ebef9d60dc8
tei-hibon <remarkabe0416@yahoo.co.jp>
2020-06-03 11:55:56 +0900

    test

--- Unisilo/src/model/AnalysisBusinessLogic.java
+++ Unisilo/src/model/AnalysisBusinessLogic.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 /**----------------------------------------------------------------------*
@@ -11,16 +9,16 @@ import java.util.Map;
 public class AnalysisBusinessLogic {
 
 	//DAOクラス
-	private AnalysisDao dao = new AnalysisDao();
-
+//	private AnalysisDao dao = new AnalysisDao();
+	private SampleAnalysisDao dao = new SampleAnalysisDao();
 
 
 	/**
 	 * <p>商品の一覧を返す.</p>
 	 * @return 検索結果
 	 */
-	public List<Map<String,Object>> getItems(){
-		List<Map<String,Object>> items=  dao.getItems();
+/*	public List<Map<String,Object>> getItem_name(){
+		List<Map<String,Object>> item_name=  dao.getItem_name();
 
 		//★★データベースからデータの取得
 		List<Map<String,Object>> resultList = new ArrayList<Map<String,Object>>();
@@ -36,11 +34,9 @@ public class AnalysisBusinessLogic {
 		}
 		//★★整形した結果を返す
 		return resultList;
-	}
+	}/
+*/
 
-	/**
-	 * <p>売上と利益を返す</p>
-	 */
 	public Map<String,Integer> getCalculationResult() {
 		return dao.getCalculationResult();
 	}
